dcb7b6e82037600c47ff20397ca14baf0856ae72
Matheus Trindade <trindadematheus27@gmail.com>
2019-08-04 22:47:08 -0300

    Implementando gerador de QRCode

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const qr = require('qr-image');
+
+const app = express();
+const router = express.Router();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+    res.render('index.html');
+});
+
+io.on('connection', socket => {
+    console.log(`Socket conectado ${socket.id}`);
+
+    socket.on('sendInfo', info => {
+        let code = qr.imageSync(info, { type: 'png' });
+        
+        socket.emit('returnInfo', { info, code });
+    });
+});
+
+router.get('/qr', function () {
+    var code = qr.image('http://www.google.com', { type: 'png' });
+    res.type('png');
+    code.pipe(res);
+});
+
+
+server.listen(3000, () => {
+    console.log('Server iniciado com sucesso!');
+});
